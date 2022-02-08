c366acada3427261519ccea0edfd4029226a9b3f
B9ary01 <bishalrai111@yahoo.com>
2022-02-08 23:32:45 +0000

    added products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import data from "./data";
+import Product from "./components/Product";
 
 function App() {
   return (
@@ -22,25 +23,7 @@ function App() {
     <div className="row center">
       {
         data.products.map(product => (
-       <div key={product._id} className="card">
-        <a href={`/product/${product._id}`}>
-          <img className="medium" src={product.image} alt={product.name} />
-        </a>
-        <div className="card-body">
-        <a href={`/product/${product._id}`}>
-            <h2>{product.name}</h2>
-          </a>
-          <div className="rating">
-            <span> <i className="fa fa-star"></i> </span>
-            <span> <i className="fa fa-star"></i> </span>
-            <span> <i className="fa fa-star"></i> </span>
-            <span> <i className="fa fa-star"></i> </span>
-            <span> <i className="fa fa-star"></i> </span>
-          </div>
-          <div className="price">${product.price}</div>
-        </div>
-      </div>
-      
+       <Product key={product._id} product={product}></Product>
         ))
       }
       
