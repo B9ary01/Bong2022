140f4d2222d034a7ddad7c7d0b5c213f34740b41
B9ary01 <bishalrai111@yahoo.com>
2022-02-09 14:14:30 +0000

    react redux, state added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,24 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Product from "../components/Product";
-import axios from "axios";
+import LoadingPage from '../components/LoadingPage';
+import MessagePage from '../components/MessagePage';
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/ProductActions";
 
 export default function HomeScreen() {
-  const [products, setProduct] = useState([]);
+  const dispatch= useDispatch();
+  const productList = useSelector( (state)=> state.productList);
+  const {loading,error, products}= productList;
+
   useEffect(()=>{
-  const fetchData= async () =>{
-    const {data} = await axios.get('/api/products');
-    setProduct(data);
-  };
-  fetchData();
-  }, [])
+   dispatch(listProducts());
+  }, [dispatch]);
 
     return (
       <div>
+         {loading ? (
+        <LoadingPage></LoadingPage>
+      ) : error ? (
+        <MessagePage variant="danger">{error}</MessagePage>
+      ) : (
         <div className="row center">
           {products.map((product) => (
             <Product key={product._id} product={product}></Product>
           ))}
         </div>
+      )}
       </div>
     );
   }
