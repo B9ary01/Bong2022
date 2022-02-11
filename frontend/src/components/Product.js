ca6b6060a026a260d1588835458407ee610a3aaa
B9ary01 <bishalrai111@yahoo.com>
2022-02-11 11:46:04 +0000

    add to cart files added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Rating from './Rating';
 
@@ -7,13 +8,14 @@ export default function Product(props){
 
     return(
     <div key={product._id} className="card">
-    <a href={`/product/${product._id}`}>
+      
+    <Link to={`/product/${product._id}`}>
       <img className="medium" src={product.image} alt={product.name} />
-    </a>
+    </Link>
     <div className="card-body">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <h2>{product.name}</h2>
-      </a>
+      </Link>
       <Rating
         rating={product.rating}
         numReviews={product.numReviews}
